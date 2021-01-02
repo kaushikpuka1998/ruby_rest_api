081bef431c15544dc2cd570627f38dab61411567
kaushikpuka1998 <kaushikghosh199832@gmail.com>
2021-01-03 03:52:08 +0530

    short_the_all_json_response_by_using_model_response.rb_in_concerns

--- app/controllers/api/v1/registrations_controller.rb
+++ app/controllers/api/v1/registrations_controller.rb
@@ -4,22 +4,9 @@ before_action :ensure_params_exist,only: :create
   def create
     user = User.new user_params
     if user.save
-      render json:{
-        messages:"Signed Up Successfully",
-        is_success:true,
-        data:{
-          user:user
-        }
-
-      },status: :ok
-
+      json_response "Signed Up Successfully", true, {user: user}, :ok
     else
-        render json:{
-            messages:"Something Wrong",
-            is_success: false,
-            data:{}
-
-         }, status: :unprocessable_entity
+      json_response "Something Went Wrong", false, {}, :unprocessable_entity
     end
   end
 
@@ -30,10 +17,6 @@ before_action :ensure_params_exist,only: :create
 
   def ensure_params_exist
     return if params[:user].present?
-    render json: {
-      messages: "Missing params",
-      is_success: false,
-      data: {}
-    },status: :bad_request
+      json_response "Missing Params", false, {}, :bad_request
+    end
   end
-end
