25c8c32881a7d4da88fe94d663ac96f46e9dec89
kaushikpuka1998 <kaushikghosh199832@gmail.com>
2021-01-05 04:01:59 +0530

    database slight_change and log_out_function updated

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -2,6 +2,8 @@ class Api::V1::SessionsController < Devise::SessionsController
 
   before_action :sign_in_params, only: :create
   before_action :load_user, only: :create
+  before_action :valid_token, only: :destroy
+  skip_before_action :verify_signed_out_user, only: :destroy
 
   def create
     if @user.valid_password?(sign_in_params[:password])
@@ -11,6 +13,14 @@ class Api::V1::SessionsController < Devise::SessionsController
       json_response "Unauthorized",false,{},:unauthorized
 
     end
+  end
+
+
+  def destroy
+    sign_out @user
+
+    json_response "Successfully Logged out",true,{}, :ok
+
   end
 
   #made_a_sessioncontroller class and updated post func in routes.
@@ -27,6 +37,16 @@ class Api::V1::SessionsController < Devise::SessionsController
       return @user
     else
       json_response "Cannot find User", false, {}, :unprocessable_entity
+    end
+  end
+
+
+  def valid_token
+    @user = User.find_by authentication_token: request.headers["AUTH-TOKEN"]
+    if @user
+      return @user
+    else
+      json_response "Invalid Token", false, {},:unprocessable_entity
     end
   end
 end
